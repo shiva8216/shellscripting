d37071ddcb27cc78ecd83ec662e6ccce21f49197
shiva <shiva.aws@gmail.com>
2025-06-11 06:56:55 +0530

    its practice scripting

--- shell-script/07-datatypes.sh
+++ shell-script/07-datatypes.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-NUMBER1=$1
-NUMBER2=$2
+# NUMBER1=$1
+# NUMBER2=$2
+
+# TIMESTAMP=$(date)
+# echo "script excuted at: $TIMESTAMP"
+# SUM=$(($NUMBER1+$NUMBER2))
+
+# echo "sum of $NUMBER1 and $NUMBER2 is: $SUM"
+
 
 TIMESTAMP=$(date)
 echo "script excuted at: $TIMESTAMP"
-SUM=$(($NUMBER1+$NUMBER2))
 
-#echo "sum of $NUMBER1 and $NUMBER2 is: $SUM"
+Filesystems=$(df -h)
+
+echo " to display filesystems: $filesystems
