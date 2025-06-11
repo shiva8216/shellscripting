6ff2f1b1fad492fc732fc6c38d223414ab60f6f3
shiva <shiva.aws@gmail.com>
2025-06-11 06:51:09 +0530

    its practice scripting

--- shell-script/07-datatypes.sh
+++ shell-script/07-datatypes.sh
@@ -4,5 +4,7 @@ NUMBER1=$1
 NUMBER2=$2
 
 TIMESTAMP=$(date)
-echo "sript excuted at: $TIMESTAMP"
+echo "script excuted at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
+
+echo "sum of $NUMBER1 and $NUMBER2 is: $SUM"
