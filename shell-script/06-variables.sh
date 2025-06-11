fdc37470ed4117fab342ab189b4d69d52586c340
shiva <shiva.aws@gmail.com>
2025-06-11 06:44:26 +0530

    its practice scripting

--- shell-script/06-variables.sh
+++ shell-script/06-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter username::"
-read -s username
+read -s username # -s used for security  to unvisible
 echo "username entered: $username"
 echo "Please enter your password::"
 read -s PASSWORD
