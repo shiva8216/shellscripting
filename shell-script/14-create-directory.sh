130fcf0d86cb72f4aa00d95b9ef0d54e0f76de29
shiva <shiva.aws@gmail.com>
2025-06-11 11:24:55 +0530

    shell scripting disk usage

--- shell-script/14-create-directory.sh
+++ shell-script/14-create-directory.sh
@@ -6,7 +6,7 @@ if [ $? -ne 0 ]
     echo "mallik directory not found. Creating..."
     mkdir -p /var/tmp/mallik > /dev/null 2>&1
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
     then 
         echo "creating mallik directory ....Failure"
         exit 1
@@ -18,23 +18,3 @@ if [ $? -ne 0]
 #      echo "mallik directory is ...already therere"
 #     fi
 
-
-
-
-
-
-# dnf list installed git > /dev/null 2>&1
-
-# if [ $? -ne 0 ]; then
-#     echo "Git is not installed. Installing..."
-#     dnf install git -y > /dev/null 2>&1
-
-#     if [ $? -ne 0 ]; then
-#         echo "Installing Git ... FAILURE"
-#         exit 1
-#     else
-#         echo "Installing Git ... SUCCESS"
-#     fi
-# else
-#     echo "Git is already ... INSTALLED"
-# fi
