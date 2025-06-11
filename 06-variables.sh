f2fedf2c16a965227b4c89758827b507147d542e
shiva <shiva.aws@gmail.com>
2025-06-11 06:37:05 +0530

    its practice scripting

---
+++ 06-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Please enter username::"
+read username
+
--- shell-script/05-variables.sh
+++ shell-script/05-variables.sh
@@ -7,3 +7,5 @@ echo "$PERSON1:: Hi $PERSON2"
 echo "${PERSON2}:: Hello $PERSON1"
 echo "$PERSON1:: How are you doing?"
 echo "$PERSON2:: I am good. How are you?"
+
+#refference to 04 script from joindevops
