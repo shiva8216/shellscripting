34f9203e6a7035b1dec8b52f89134995372ed24e
shiva <shiva.aws@gmail.com>
2025-06-11 11:27:39 +0530

    shell scripting disk usage

--- shell-script/14-create-directory.sh
+++ shell-script/14-create-directory.sh
@@ -2,7 +2,8 @@
 
 ls -ld /var/tmp/mallik > /dev/null 2>&1
 
-if [ $? -ne 0 ]
+if [ $? -ne 0 ] 
+then
     echo "mallik directory not found. Creating..."
     mkdir -p /var/tmp/mallik > /dev/null 2>&1
 
@@ -11,5 +12,8 @@ if [ $? -ne 0 ]
         echo "creating mallik directory .... Failure"
         exit 1
     else 
+        echo "creating  mallik directory .... Success"
+    fi
+else 
         echo "creating  mallik directory .... Success"
     fi
