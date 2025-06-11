d5720771683fab100db1c9625e4c11c8043aa08e
shiva <shiva.aws@gmail.com>
2025-06-11 06:20:02 +0530

    its practice scripting

--- shell-script/02-variable.sh
+++ shell-script/02-variable.sh
@@ -5,7 +5,19 @@ echo "Robert:: Hello Rahim"
 echo "Rahim:: hov are your Robert"
 echo "Robert:: i am good Rahim"
 echo "Rahim::   are you doing any job Robert"
-echo "Robert::  yes  Rahim ,i am doing "
+echo "Robert::  yes  Rahim ,i am doing"
+
+
+
+
+# output:
+
+# Rahim:: Hi Robert
+# Robert:: Hello Rahim
+# Rahim:: hov are your Robert
+# Robert:: i am good Rahim
+# Rahim::   are you doing any job Robert
+# Robert::  yes  Rahim ,i am doing
 
 
 
@@ -35,10 +47,3 @@ echo "Robert::  yes  Rahim ,i am doing "
 
 
 
-
-
-
-
-
-
-"
