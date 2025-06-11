9482fc6708fc648ebe37901aaabb36a9aec305cc
shiva <shiva.aws@gmail.com>
2025-06-11 10:20:16 +0530

    shell scripting disk usage

---
+++ shell-script/11-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt , -lt, -eq, -ge, -le
+
+if  [ $NUMBER -gt 100 ]
+
+then
+    echo "given number is greater than equal 100"
+
+else 
+    echo "given number is lessthan or equal 100"
+
+fi
