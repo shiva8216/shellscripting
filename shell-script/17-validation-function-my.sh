29295ddfca517be16dc02a075edca28d1eed2fb9
shiva <shiva.aws@gmail.com>
2025-06-11 15:34:59 +0530

    shell scripting disk usage

--- shell-script/17-validation-function-my.sh
+++ shell-script/17-validation-function-my.sh
@@ -10,8 +10,8 @@ validate() {
 }
 
 echo "Creating directory..."
-mkdir /tmp/test123 > /dev/null 2>&1
-validate $? "Creating /tmp/test123"
+mkdir /tmp/sj > /dev/null 2>&1
+validate $? "Creating /tmp/sj"
 
 echo "Installing Git..."
 dnf install git -y > /dev/null 2>&1
