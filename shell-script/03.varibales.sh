6ead499a7b27c4f744040d88a3765de3a74c71d3
shiva <shiva.aws@gmail.com>
2025-06-11 06:27:33 +0530

    its practice scripting

--- shell-script/03.varibales.sh
+++ shell-script/03.varibales.sh
@@ -7,3 +7,12 @@ echo "$person1:: Hi $person2"
 echo "$person2:: Hello $person1"
 echo "$person1:: Hov are you $person2"
 echo "$person2::  i am good $person1"
+
+
+# output
+
+# Suresh:: Hi ramya
+# ramya:: Hello Suresh
+# Suresh:: Hov are you ramya
+# ramya::  i am good Suresh
+
---
+++ shell-script/04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hello $person1"
+echo "$person1:: are you preparing for TSPSC $person2"
+echo "$person2:: yes its for Group2 $person1"
