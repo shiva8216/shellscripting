24fed98c87ecd83daee48ed84fe9c347c9e97827
shiva <shiva.aws@gmail.com>
2025-06-11 06:31:37 +0530

    its practice scripting

--- shell-script/04-variables.sh
+++ shell-script/04-variables.sh
@@ -7,3 +7,11 @@ echo "$person1:: Hi $person2"
 echo "$person2:: Hello $person1"
 echo "$person1:: are you preparing for TSPSC $person2"
 echo "$person2:: yes its for Group2 $person1"
+
+
+# output:
+
+# john:: Hi robert
+# robert:: Hello john
+# john:: are you preparing for TSPSC robert
+# robert:: yes its for Group2 john
