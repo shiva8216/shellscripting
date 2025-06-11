45cd9d0dd992183b42b98d7fd542badebc55d6c2
shiva <shiva.aws@gmail.com>
2025-06-11 09:49:17 +0530

    shell scripting disk usage

--- shell-script/09-specrial-vars.sh
+++ shell-script/09-specrial-vars.sh
@@ -9,3 +9,15 @@ echo "Which user is running this script: $USER"
 echo "Process id of current script: $$"
 sleep 60 &
 echo "Process id of last command in background: $!"
+
+# output:
+
+# [ root@ip-172-31-93-134 ~/shellscripting/shell-script ]# sh 09-specrial-vars.sh  12 23 4 5 6
+# All variables passed: 12 23 4 5 6
+# Number of variables: 5
+# Script name: 09-specrial-vars.sh
+# Present working directory: /root/shellscripting/shell-script
+# Home directory of current user: /root
+# Which user is running this script: root
+# Process id of current script: 2785
+# Process id of last command in background: 2786
---
+++ shell-script/10-disk-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get current date and hostname
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+HOST=$(hostname)
+
+echo "Disk Usage Report for $HOST on $DATE"
+echo "-------------------------------------"
+
+# Show overall disk usage
+df -hT | awk 'NR==1 || $2=="ext4" || $2=="xfs" || $2=="btrfs"'
+
+echo ""
+echo "Top 10 largest directories in / (root):"
+du -ah / | sort -rh | head -n 10
