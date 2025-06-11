04453ed405b6466a8e482861e76600042358fc0f
shiva <shiva.aws@gmail.com>
2025-06-11 10:07:13 +0530

    shell scripting disk usage

--- shell-script/10-disk-usage.sh
+++ shell-script/10-disk-usage.sh
@@ -4,15 +4,18 @@
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 HOST=$(hostname)
 
-TIMESTAMP=$(date)
-echo "script excuted at: $TIMESTAMP"
+# TIMESTAMP=$(date)
+# echo "script excuted at: $TIMESTAMP"
 
- echo "Disk Usage Report for $HOST on $DATE"
- echo "-------------------------------------"
+#  echo "Disk Usage Report for $HOST on $DATE"
+#  echo "-------------------------------------"
 
-# Show overall disk usage
-df -hT | awk 'NR==1 || $2=="ext4" || $2=="xfs" || $2=="btrfs"'
+# # Show overall disk usage
+# df -hT | awk 'NR==1 || $2=="ext4" || $2=="xfs" || $2=="btrfs"'
 
-echo "--------------------"
-echo "Top 10 largest directories in / (root):"
-du -ah / | sort -rh | head -n 10
+# echo "--------------------"
+# echo "Top 10 largest directories in / (root):"
+# du -ah / | sort -rh | head -n 10
+
+
+df -hT | awk 'NR==1 
