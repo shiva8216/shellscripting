e64c40e01db6667c2efe2e47fe34dbebbb24acfa
shiva <shiva.aws@gmail.com>
2025-06-11 10:15:05 +0530

    shell scripting disk usage

--- shell-script/10-disk-usage.sh
+++ shell-script/10-disk-usage.sh
@@ -7,15 +7,34 @@ HOST=$(hostname)
 # TIMESTAMP=$(date)
 # echo "script excuted at: $TIMESTAMP"
 
-#  echo "Disk Usage Report for $HOST on $DATE"
-#  echo "-------------------------------------"
+ echo "Disk Usage Report for $HOST on $DATE"
+  echo "-------------------------------------"
 
 # # Show overall disk usage
-# df -hT | awk 'NR==1 || $2=="ext4" || $2=="xfs" || $2=="btrfs"'
+ df -hT | awk 'NR==1 || $2=="ext4" || $2=="xfs" || $2=="btrfs"'
 
-# echo "--------------------"
-# echo "Top 10 largest directories in / (root):"
-# du -ah / | sort -rh | head -n 10
+ echo "--------------------"
+ echo "Top 10 largest directories in / (root):"
+ du -ah / | sort -rh | head -n 10
 
 
-df -hT | awk 'NR==2'
+
+
+
+# out put:
+
+# df -hT: Shows disk usage with:
+
+# -h: human-readable sizes (e.g., GB, MB)
+
+# -T: includes the filesystem type (e.g., ext4, xfs)
+
+# awk '...': Processes the output line-by-line:
+
+# NR==1: Match the first line (header)
+
+# $2=="ext4": Match lines where the 2nd column (filesystem type) is "ext4"
+
+# ||: Logical OR – include the line if any condition is true
+
+
